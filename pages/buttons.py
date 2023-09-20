3bc4cda4e879f51b86e68e3c237888f882c53c5d
Zomlik <sanek77594@yandex.ru>
2023-09-20 05:45:36 +0300

    New class

--- pages/buttons.py
+++ pages/buttons.py
@@ -2,6 +2,7 @@ from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 LOCATER_SIMPLE_BUTTON = (By.ID, "submit-id-submit")
+LOCATER_LOOK_LIKE_BUTTON = (By.CSS_SELECTOR, ".a-button")
 
 
 class SimpleButtons(BasePage):
@@ -16,4 +17,19 @@ class SimpleButtons(BasePage):
 
     def simple_button_label(self):
         return self.simple_button_find().get_attribute('value')
+
+
+class LookLikeButton(BasePage):
+    def __init__(self, browser):
+        super().__init__(browser)
+
+    def like_button_find(self):
+        return self.find(LOCATER_LOOK_LIKE_BUTTON)
+
+    def like_button_click(self):
+        return self.like_button_find().click()
+
+    def like_button_label(self):
+        return self.like_button_find().text
+
 
