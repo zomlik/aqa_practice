87a4caf69e00cb22cf04cca223692eb884bbb792
Zomlik <sanek77594@yandex.ru>
2023-09-23 07:31:37 +0300

    added allure

--- test_look_like_button.py
+++ test_look_like_button.py
@@ -1,23 +1,33 @@
 from pages.buttons import LookLikeButton
+import allure
 
 URL = 'https://www.qa-practice.com/elements/button/like_a_button'
 
 
+@allure.feature("Look like button")
+@allure.title("Look like button click")
 def test_look_like_button_click(browser):
     click_like_button = LookLikeButton(browser)
     click_like_button.go_to(URL)
     click_like_button.like_button_find()
     click_like_button.like_button_click()
-    assert click_like_button.like_button_find().text == "Click"
+    with allure.step("Label button is 'Click'"):
+        assert click_like_button.like_button_find().text == "Click"
 
 
+@allure.feature("Look like button")
+@allure.title("Look like button label")
 def test_look_like_button_label(browser):
     lebel_like_button = LookLikeButton(browser)
     lebel_like_button.go_to(URL)
-    assert lebel_like_button.like_button_label() == "Click"
+    with allure.step("Button is clicked"):
+        assert lebel_like_button.like_button_label() == "Click"
 
 
+@allure.feature("Look like button")
+@allure.title("Look like button is enabled")
 def test_look_like_button_is_enable(browser):
     enable_like_button = LookLikeButton(browser)
     enable_like_button.go_to(URL)
-    assert enable_like_button.like_button_find().is_enabled()
+    with allure.step("button is enabled"):
+        assert enable_like_button.like_button_find().is_enabled()
