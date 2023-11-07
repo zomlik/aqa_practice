2592fe55b925c9809db9930894f2e68f5fe23a65
Zomlik <sanek77594@yandex.ru>
2023-11-07 07:37:59 +0300

    added chrome options

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 
 @pytest.fixture()
-def browser():
-    chrome_browser = webdriver.Chrome()
+def chrome_option():
+    option = Options()
+    option.add_argument("--headless")
+    return option
+
+
+@pytest.fixture()
+def browser(chrome_option):
+    chrome_browser = webdriver.Chrome(chrome_option)
     chrome_browser.implicitly_wait(10)
     yield chrome_browser
     chrome_browser.quit()
